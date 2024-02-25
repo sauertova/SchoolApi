4eb72537efcdb78c8a3621dd200217605deb28ed
Tova Sauer <sauertova1@gmail.com>
2024-02-25 12:29:32 -0500

    added pool

--- server/db/index.js
+++ server/db/index.js
@@ -4,5 +4,8 @@ require('dotenv').config()
 
 const pool = new pg.Pool({
     user: process.env.PG_USERNAME,
-    
+    host: 'localhost',
+    database: 'school',
+    password: process.env.PG_PASSWORD,
+    port: 5432
 })
